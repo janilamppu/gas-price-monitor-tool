18c05e27f15744723f418380f8e266ccf8d3b376
Jani Lamppu <jani.lamppu@gmail.com>
2021-04-22 00:16:50 +0300

    Add CORS

--- src/api.js
+++ src/api.js
@@ -2,15 +2,32 @@
 const { getDataFromS3 } = require('./services/dataService');
 
 module.exports.handler = async (event, context, callback) => {
-  const data = await getDataFromS3();
+  try {
+    const data = await getDataFromS3();
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(data),
-  };
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
 
-  // print response
-  console.log(response);
+    // print response
+    console.log(response);
 
-  callback(null, response);
+    callback(null, response);
+  } catch (err) {
+    const response = {
+      statusCode: 500,
+      body: err.message,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+
+    callback(null, response);
+  }
 };
